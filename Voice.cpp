349a378d49adaf12bef50e4c483fbefaec0d4871
MothSynths <mothsynthcontact@gmail.com>
2024-11-09 18:57:04 -0500

    change to volume setting, f1 mutes, f2 cycles through low hi

--- Voice.cpp
+++ Voice.cpp
@@ -125,15 +125,6 @@ int Voice::UpdateVoice() {
   }
 
   if (volumeNum == 3) {
-    int limit = 3000;
-    if (abs(sample) > limit) {
-      if (sample > 0) {
-        sample = limit;
-      } else {
-        sample = limit * -1;
-      }
-      sample = (sample * 3);
-    }
   }
 
   if (lowPassMult > 0) {
@@ -496,10 +487,14 @@ void Voice::SetVolume(int val) {
   volumeNum = val;
   switch (val) {
     case 0:
-      volume = 1;
+      volume = 0;
       break;
     case 1:
-      volume = 2;
+      if (volume == 2) {
+        volume = 3;
+      } else {
+        volume = 2;
+      }
       break;
     case 2:
       volume = 3;
